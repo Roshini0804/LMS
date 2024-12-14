0ea919ccf005ba2bd542b3e36865acdc73b56fcf
Roshini0804 <roshinishaik2022@gmail.com>
2024-12-14 06:23:18 -0600

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,7 @@
 <?php
 
-    $con = mysqli_connect("127.0.0.1","root","","LibraryManagementSystem");
+    //$con = mysqli_connect("127.0.0.1","root","","LibraryManagementSystem");
+$con = mysqli_connect("otmaa16c1i9nwrek.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "llgqo5z0vngqvpm4", "jpxdxe8tmanop4uc", "xi7dmfa3orhj8n9k");
     // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
